77fa127d0bde17f6ac9fa10df175efbe45fe53c7
David Ekunno <matramere@gmail.com>
2023-09-04 20:07:45 -0500

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 
 # instanciating app in Flask class
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+from .models import db
+db._init_(app)
+
+CORS(app)
 
 # this line of code - directs the flask app to routes for web routes
 from . import routes
